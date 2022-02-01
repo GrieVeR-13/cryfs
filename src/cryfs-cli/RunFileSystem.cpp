bc750b79e7dc79a2c718ae687c1813279e00d16c
GrieVeR-13 <GrieVeR-C@list.ru>
2022-02-01 11:09:06 +0300

    Ren: cryfs-jni

--- src/cryfs-cli/RunFileSystem.cpp
+++ src/cryfs-cli/RunFileSystem.cpp
@@ -1,13 +1,12 @@
 #include "RunFileSystem.h"
 #include "cryfs-cli/Cli.h"
 
-
 int RunFileSystem::run() {
     int argc = 3;
     const char *argv[] = {"cryfs", "basedir", "mountdir"};
     try {
         auto &keyGenerator = cpputils::Random::OSRandom();
-        cryfs_cli::Cli(keyGenerator, cpputils::SCrypt::DefaultSettings).main(argc, argv, [] {});
+//        cryfs_cli::Cli(keyGenerator, cpputils::SCrypt::DefaultSettings).main(argc, argv, [] {});
         return 0;
     } catch (const std::exception &e) {
         return -1;
