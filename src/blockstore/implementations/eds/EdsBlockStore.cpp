fe7107c3252a4197cd35f1a517ee27110114cc19
GrieVeR-13 <GrieVeR-C@list.ru>
2022-02-01 14:44:42 +0300

    Upd: EdsBlockStore (step 1)

--- src/blockstore/implementations/eds/EdsBlockStore.cpp
+++ src/blockstore/implementations/eds/EdsBlockStore.cpp
@@ -6,41 +6,145 @@ using boost::none;
 using cpputils::Data;
 
 namespace blockstore {
-namespace eds {
+    namespace eds {
 
-EdsBlockStore::EdsBlockStore(std::string path)
-{
+        const string EdsBlockStore::FORMAT_VERSION_HEADER_PREFIX = "cryfs;block;";
+        const string EdsBlockStore::FORMAT_VERSION_HEADER = EdsBlockStore::FORMAT_VERSION_HEADER_PREFIX + "0";
+        namespace {
+            constexpr size_t PREFIX_LENGTH = 3;
+//            constexpr size_t POSTFIX_LENGTH = BlockId::STRING_LENGTH - PREFIX_LENGTH;
+//            constexpr const char *ALLOWED_BLOCKID_CHARACTERS = "0123456789ABCDEF";
+        }
 
-}
+        boost::filesystem::path EdsBlockStore::_getFilepath(const BlockId &blockId) const {
+            std::string blockIdStr = blockId.ToString();
+            return _rootDir / blockIdStr.substr(0, PREFIX_LENGTH) / blockIdStr.substr(PREFIX_LENGTH);
+        }
 
-bool EdsBlockStore::tryCreate(const BlockId &blockId, const Data &data) {
-  return true;
-}
+//        Data EdsBlockStore::_checkAndRemoveHeader(const Data &data) {
+//            if (!_isAcceptedCryfsHeader(data)) {
+//                if (_isOtherCryfsHeader(data)) {
+//                    throw std::runtime_error(
+//                            "This block is not supported yet. Maybe it was created with a newer version of CryFS?");
+//                } else {
+//                    throw std::runtime_error("This is not a valid block.");
+//                }
+//            }
+//            Data result(data.size() - formatVersionHeaderSize());
+//            std::memcpy(result.data(), data.dataOffset(formatVersionHeaderSize()), result.size());
+//            return result;
+//        }
 
-bool EdsBlockStore::remove(const BlockId &blockId) {
-  return true;
-}
+//        bool EdsBlockStore::_isAcceptedCryfsHeader(const Data &data) {
+//            return 0 == std::memcmp(data.data(), FORMAT_VERSION_HEADER.c_str(), formatVersionHeaderSize());
+//        }
+//
+//        bool EdsBlockStore::_isOtherCryfsHeader(const Data &data) {
+//            return 0 == std::memcmp(data.data(), FORMAT_VERSION_HEADER_PREFIX.c_str(), FORMAT_VERSION_HEADER_PREFIX.size());
+//        }
 
-optional<Data> EdsBlockStore::load(const BlockId &blockId) const {
-//  return _checkAndRemoveHeader(*fileContent);
-}
+        unsigned int EdsBlockStore::formatVersionHeaderSize() {
+            return FORMAT_VERSION_HEADER.size() + 1; // +1 because of the null byte
+        }
 
-void EdsBlockStore::store(const BlockId &blockId, const Data &data) {
-}
+        EdsBlockStore::EdsBlockStore(const boost::filesystem::path &path)
+                : _rootDir(path) {}
 
-uint64_t EdsBlockStore::numBlocks() const {
-//  return count;
-}
+        bool EdsBlockStore::tryCreate(const BlockId &blockId, const Data &data) {
+            auto filepath = _getFilepath(blockId);
+//            if (boost::filesystem::exists(filepath)) {
+//                return false; //todoe
+//            }
 
-uint64_t EdsBlockStore::estimateNumFreeBytes() const {
-}
+            store(blockId, data);
+            return true;
+        }
 
-uint64_t EdsBlockStore::blockSizeFromPhysicalBlockSize(uint64_t blockSize) const {
-}
+        bool EdsBlockStore::remove(const BlockId &blockId) {
+            auto filepath = _getFilepath(blockId);
+//            if (!boost::filesystem::is_regular_file(filepath)) { // TODO Is this branch necessary?
+//                return false;
+//            }
+//            bool retval = boost::filesystem::remove(filepath);
+//            if (!retval) {
+//                cpputils::logging::LOG(cpputils::logging::ERR, "Couldn't find block {} to remove", blockId.ToString());
+//                return false;
+//            }
+//            if (boost::filesystem::is_empty(filepath.parent_path())) {
+//                boost::filesystem::remove(filepath.parent_path());
+//            }
+            return true;
+        }
 
-void EdsBlockStore::forEachBlock(std::function<void (const BlockId &)> callback) const {
+        optional<Data> EdsBlockStore::load(const BlockId &blockId) const {
+            auto fileContent = Data::LoadFromFile(_getFilepath(blockId));
+//            if (fileContent == none) {
+//                return boost::none;
+//            }
+//            return _checkAndRemoveHeader(*fileContent);
+        }
 
-}
+        void EdsBlockStore::store(const BlockId &blockId, const Data &data) {
+            Data fileContent(formatVersionHeaderSize() + data.size());
+            std::memcpy(fileContent.data(), FORMAT_VERSION_HEADER.c_str(), formatVersionHeaderSize());
+            std::memcpy(fileContent.dataOffset(formatVersionHeaderSize()), data.data(), data.size());
+            auto filepath = _getFilepath(blockId);
+//            boost::filesystem::create_directory( //todoe
+//                    filepath.parent_path()); // TODO Instead create all of them once at fs creation time?
+            fileContent.StoreToFile(filepath);
+        }
 
-}
+        uint64_t EdsBlockStore::numBlocks() const {
+//            uint64_t count = 0;
+//            for (auto prefixDir = boost::filesystem::directory_iterator(_rootDir);
+//                 prefixDir != boost::filesystem::directory_iterator(); ++prefixDir) {
+//                if (boost::filesystem::is_directory(prefixDir->path())) {
+//                    count += std::distance(boost::filesystem::directory_iterator(prefixDir->path()),
+//                                           boost::filesystem::directory_iterator());
+//                }
+//            }
+//            return count;
+        }
+
+        uint64_t EdsBlockStore::estimateNumFreeBytes() const {
+//	return cpputils::free_disk_space_in_bytes(_rootDir);
+            return 0;
+        }
+
+        uint64_t EdsBlockStore::blockSizeFromPhysicalBlockSize(uint64_t blockSize) const {
+            if (blockSize <= formatVersionHeaderSize()) {
+                return 0;
+            }
+            return blockSize - formatVersionHeaderSize();
+        }
+
+        void EdsBlockStore::forEachBlock(std::function<void(const BlockId &)> callback) const {
+//            for (auto prefixDir = boost::filesystem::directory_iterator(_rootDir);
+//                 prefixDir != boost::filesystem::directory_iterator(); ++prefixDir) {
+//                if (!boost::filesystem::is_directory(prefixDir->path())) {
+//                    continue;
+//                }
+//
+//                std::string blockIdPrefix = prefixDir->path().filename().string();
+//                if (blockIdPrefix.size() != PREFIX_LENGTH ||
+//                    std::string::npos != blockIdPrefix.find_first_not_of(ALLOWED_BLOCKID_CHARACTERS)) {
+//                    // directory has wrong length or an invalid character
+//                    continue;
+//                }
+//
+//                for (auto block = boost::filesystem::directory_iterator(prefixDir->path());
+//                     block != boost::filesystem::directory_iterator(); ++block) {
+//                    std::string blockIdPostfix = block->path().filename().string();
+//                    if (blockIdPostfix.size() != POSTFIX_LENGTH ||
+//                        std::string::npos != blockIdPostfix.find_first_not_of(ALLOWED_BLOCKID_CHARACTERS)) {
+//                        // filename has wrong length or an invalid character
+//                        continue;
+//                    }
+//
+//                    callback(BlockId::FromString(blockIdPrefix + blockIdPostfix));
+//                }
+//            }
+        }
+
+    }
 }
--- src/blockstore/implementations/eds/EdsBlockStore.h
+++ src/blockstore/implementations/eds/EdsBlockStore.h
@@ -1,6 +1,6 @@
 #pragma once
-#ifndef MESSMER_BLOCKSTORE_IMPLEMENTATIONS_EDS_ONDISKBLOCKSTORE_H_
-#define MESSMER_BLOCKSTORE_IMPLEMENTATIONS_EDS_ONDISKBLOCKSTORE_H_
+#ifndef BLOCKSTORE_IMPLEMENTATIONS_EDS_ONDISKBLOCKSTORE_H_
+#define BLOCKSTORE_IMPLEMENTATIONS_EDS_ONDISKBLOCKSTORE_H_
 
 #include "../../interface/BlockStore2.h"
 #include <cpp-utils/macros.h>
@@ -8,27 +8,45 @@
 #include <cpp-utils/logging/logging.h>
 
 namespace blockstore {
-namespace eds {
+    namespace eds {
 
-class EdsBlockStore final: public BlockStore2 {
-public:
-  EdsBlockStore(std::string path);
+        class EdsBlockStore final : public BlockStore2 {
+        public:
+            EdsBlockStore(const boost::filesystem::path &path);
 
-  bool tryCreate(const BlockId &blockId, const cpputils::Data &data) override;
-  bool remove(const BlockId &blockId) override;
-  boost::optional<cpputils::Data> load(const BlockId &blockId) const override;
-  void store(const BlockId &blockId, const cpputils::Data &data) override;
-  uint64_t numBlocks() const override;
-  uint64_t estimateNumFreeBytes() const override;
-  uint64_t blockSizeFromPhysicalBlockSize(uint64_t blockSize) const override;
-  void forEachBlock(std::function<void (const BlockId &)> callback) const override;
+            bool tryCreate(const BlockId &blockId, const cpputils::Data &data) override;
 
-private:
+            bool remove(const BlockId &blockId) override;
 
-  DISALLOW_COPY_AND_ASSIGN(EdsBlockStore);
-};
+            boost::optional<cpputils::Data> load(const BlockId &blockId) const override;
 
-}
+            void store(const BlockId &blockId, const cpputils::Data &data) override;
+
+            uint64_t numBlocks() const override;
+
+            uint64_t estimateNumFreeBytes() const override;
+
+            uint64_t blockSizeFromPhysicalBlockSize(uint64_t blockSize) const override;
+
+            void forEachBlock(std::function<void(const BlockId &)> callback) const override;
+
+        private:
+
+            boost::filesystem::path _rootDir;
+
+            static const std::string FORMAT_VERSION_HEADER_PREFIX;
+            static const std::string FORMAT_VERSION_HEADER;
+//
+            boost::filesystem::path _getFilepath(const BlockId &blockId) const;
+//            static cpputils::Data _checkAndRemoveHeader(const cpputils::Data &data);
+//            static bool _isAcceptedCryfsHeader(const cpputils::Data &data);
+//            static bool _isOtherCryfsHeader(const cpputils::Data &data);
+            static unsigned int formatVersionHeaderSize();
+
+            DISALLOW_COPY_AND_ASSIGN(EdsBlockStore);
+        };
+
+    }
 }
 
 #endif
